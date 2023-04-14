e65cdc5a199b42488d8ca81a1aa737fc419f4c15
samimeadad <sami.meadad@gmail.com>
2023-04-14 16:35:34 +0600

    countries section done

--- src/Pages/Home/Countries.js
+++ src/Pages/Home/Countries.js
@@ -3,6 +3,19 @@ import { Card, Col, Container, Row } from 'react-bootstrap';
 import flag1 from '../../images/home/Countries/as-flag.gif';
 import flag2 from '../../images/home/Countries/au-flag.gif';
 import flag3 from '../../images/home/Countries/ca-flag.gif';
+import flag4 from '../../images/home/Countries/us-flag.gif';
+import flag5 from '../../images/home/Countries/uk-flag.gif';
+import flag6 from '../../images/home/Countries/tu-flag.gif';
+import flag7 from '../../images/home/Countries/sw-flag.gif';
+import flag8 from '../../images/home/Countries/ks-flag.gif';
+import flag9 from '../../images/home/Countries/pl-flag.gif';
+import flag10 from '../../images/home/Countries/po-flag.gif';
+import flag11 from '../../images/home/Countries/ro-flag.gif';
+import flag12 from '../../images/home/Countries/rs-flag.gif';
+import flag13 from '../../images/home/Countries/my-flag.gif';
+import flag14 from '../../images/home/Countries/sp-flag.gif';
+import flag15 from '../../images/home/Countries/ja-flag.gif';
+import flag16 from '../../images/home/Countries/ei-flag.gif';
 
 const Countries = () => {
     return (
@@ -12,7 +25,7 @@ const Countries = () => {
                 <Row className='g-5 text-center ms-4 my-3'>
                     <Col xs={ 12 } sm={ 12 } md={ 3 } lg={ 3 }>
                         <Card style={ { width: '18rem', height: "55%" } }>
-                            <Card.Img variant="top" src={ flag1 } className="img-fluid m-auto" style={ { height: '12rem' } } />
+                            <Card.Img variant="top" src={ flag1 } className="img-fluid m-auto" style={ { height: '150px' } } />
                             <Card.Body>
                                 <Card.Title className='fs-4'>Australia</Card.Title>
                             </Card.Body>
@@ -20,7 +33,7 @@ const Countries = () => {
                     </Col>
                     <Col xs={ 12 } sm={ 12 } md={ 3 } lg={ 3 }>
                         <Card style={ { width: '18rem', height: "55%" } }>
-                            <Card.Img variant="top" src={ flag2 } className="img-fluid m-auto" style={ { height: '9rem' } } />
+                            <Card.Img variant="top" src={ flag2 } className="img-fluid m-auto" style={ { height: '150px' } } />
                             <Card.Body>
                                 <Card.Title className='fs-4'>Austria</Card.Title>
                             </Card.Body>
@@ -28,7 +41,7 @@ const Countries = () => {
                     </Col>
                     <Col xs={ 12 } sm={ 12 } md={ 3 } lg={ 3 }>
                         <Card style={ { width: '18rem', height: "55%" } }>
-                            <Card.Img variant="top" src={ flag3 } className="img-fluid m-auto" style={ { height: '12rem' } } />
+                            <Card.Img variant="top" src={ flag3 } className="img-fluid m-auto" style={ { height: '150px' } } />
                             <Card.Body>
                                 <Card.Title className='fs-4'>Canada</Card.Title>
                             </Card.Body>
@@ -36,9 +49,111 @@ const Countries = () => {
                     </Col>
                     <Col xs={ 12 } sm={ 12 } md={ 3 } lg={ 3 }>
                         <Card style={ { width: '18rem', height: "55%" } }>
-                            <Card.Img variant="top" src={ flag3 } className="img-fluid m-auto" style={ { height: '12rem' } } />
+                            <Card.Img variant="top" src={ flag4 } className="img-fluid m-auto" style={ { height: '150px' } } />
                             <Card.Body>
-                                <Card.Title className='fs-4'>Australia</Card.Title>
+                                <Card.Title className='fs-4'>USA</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row className='g-5 text-center ms-4 my-3'>
+                    <Col xs={ 12 } sm={ 12 } md={ 3 } lg={ 3 }>
+                        <Card style={ { width: '18rem', height: "55%" } }>
+                            <Card.Img variant="top" src={ flag5 } className="img-fluid m-auto" style={ { height: '150px' } } />
+                            <Card.Body>
+                                <Card.Title className='fs-4'>United Kingdom</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col xs={ 12 } sm={ 12 } md={ 3 } lg={ 3 }>
+                        <Card style={ { width: '18rem', height: "55%" } }>
+                            <Card.Img variant="top" src={ flag6 } className="img-fluid m-auto" style={ { height: '150px' } } />
+                            <Card.Body>
+                                <Card.Title className='fs-4'>Turkey</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col xs={ 12 } sm={ 12 } md={ 3 } lg={ 3 }>
+                        <Card style={ { width: '18rem', height: "55%" } }>
+                            <Card.Img variant="top" src={ flag7 } className="img-fluid m-auto" style={ { height: '150px' } } />
+                            <Card.Body>
+                                <Card.Title className='fs-4'>Sweden</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col xs={ 12 } sm={ 12 } md={ 3 } lg={ 3 }>
+                        <Card style={ { width: '18rem', height: "55%" } }>
+                            <Card.Img variant="top" src={ flag8 } className="img-fluid m-auto" style={ { height: '150px' } } />
+                            <Card.Body>
+                                <Card.Title className='fs-4'>South Korea</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row className='g-5 text-center ms-4 my-3'>
+                    <Col xs={ 12 } sm={ 12 } md={ 3 } lg={ 3 }>
+                        <Card style={ { width: '18rem', height: "55%" } }>
+                            <Card.Img variant="top" src={ flag9 } className="img-fluid m-auto" style={ { height: '150px' } } />
+                            <Card.Body>
+                                <Card.Title className='fs-4'>Poland</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col xs={ 12 } sm={ 12 } md={ 3 } lg={ 3 }>
+                        <Card style={ { width: '18rem', height: "55%" } }>
+                            <Card.Img variant="top" src={ flag10 } className="img-fluid m-auto" style={ { height: '150px' } } />
+                            <Card.Body>
+                                <Card.Title className='fs-4'>Portugal</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col xs={ 12 } sm={ 12 } md={ 3 } lg={ 3 }>
+                        <Card style={ { width: '18rem', height: "55%" } }>
+                            <Card.Img variant="top" src={ flag11 } className="img-fluid m-auto" style={ { height: '150px' } } />
+                            <Card.Body>
+                                <Card.Title className='fs-4'>Romania</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col xs={ 12 } sm={ 12 } md={ 3 } lg={ 3 }>
+                        <Card style={ { width: '18rem', height: "55%" } }>
+                            <Card.Img variant="top" src={ flag12 } className="img-fluid m-auto" style={ { height: '150px' } } />
+                            <Card.Body>
+                                <Card.Title className='fs-4'>Russia</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row className='g-5 text-center ms-4 my-3'>
+                    <Col xs={ 12 } sm={ 12 } md={ 3 } lg={ 3 }>
+                        <Card style={ { width: '18rem', height: "55%" } }>
+                            <Card.Img variant="top" src={ flag13 } className="img-fluid m-auto" style={ { height: '150px' } } />
+                            <Card.Body>
+                                <Card.Title className='fs-4'>Malaysia</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col xs={ 12 } sm={ 12 } md={ 3 } lg={ 3 }>
+                        <Card style={ { width: '18rem', height: "55%" } }>
+                            <Card.Img variant="top" src={ flag14 } className="img-fluid m-auto" style={ { height: '150px' } } />
+                            <Card.Body>
+                                <Card.Title className='fs-4'>Spain</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col xs={ 12 } sm={ 12 } md={ 3 } lg={ 3 }>
+                        <Card style={ { width: '18rem', height: "55%" } }>
+                            <Card.Img variant="top" src={ flag15 } className="img-fluid m-auto" style={ { height: '150px' } } />
+                            <Card.Body>
+                                <Card.Title className='fs-4'>Japan</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col xs={ 12 } sm={ 12 } md={ 3 } lg={ 3 }>
+                        <Card style={ { width: '18rem', height: "55%" } }>
+                            <Card.Img variant="top" src={ flag16 } className="img-fluid m-auto" style={ { height: '150px' } } />
+                            <Card.Body>
+                                <Card.Title className='fs-4'>Ireland</Card.Title>
                             </Card.Body>
                         </Card>
                     </Col>
